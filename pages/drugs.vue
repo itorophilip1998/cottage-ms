2a15cbc9b79250a29f938f53875014e0881e0502
itorophilip1998 <itkeyzzpianny@gmail.com>
2022-01-01 05:05:43 +0100

    test

--- pages/drugs.vue
+++ pages/drugs.vue
@@ -1,20 +1,16 @@
 
 <template>
   <div>
-   <div class="drugsBox mt-5 pt-5 p-2">
+   <div class="drugsBox mt-5 pt-5 p-2 bg-light">
         <h5 class=" p-2  font-weight-bold text-dark">Drugs</h5>
 
          <div class="drugs row m-0 mt-3" v-for="(drug,index) in drugs.drugs" :key="index">
-            <div class="card text-center">
-              <img class="card-img-top mx-auto" :src="drug.image" alt="">
-              <div class="card-body">
-                <h4 class="card-title">{{ drug.name }} </h4>
-                <p class="card-text">{{ drug.description }}</p>
-                <h6 class="">\
-
-
-                   {{ drug.price }}</h6>
-                <!-- naira -->
+            <div class="card text-center shadow bg-white pt-3">
+              <div class="card-body text-justify">
+                <h4 class="card-title">{{ drug.name }}<i class="fa fa-tag text-danger" aria-hidden="true"></i> </h4>
+                <p class="card-text ">{{ drug.description }}</p>
+                <h3><span class=" text-dark mb-3">     <b>₦{{ drug.price }}.00</b>    </span></h3>
+                <a :href="drug.url" class="btn btn-primary w-100 " >Buy  <i class="fas fa-shopping-cart    "></i> </a>
               </div>
             </div>
          </div>
